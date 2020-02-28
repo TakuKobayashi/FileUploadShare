20b2476230db0c3616b15141f9a90a3e497bc2f6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-02-28 17:30:06 +0900

    linebot sdkとかをいれました

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-import * as functions from 'firebase-functions';
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+import { config, region } from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+
+require('dotenv').config();
+admin.initializeApp(config().firebase);
+
+import { lineBotRouter } from './routes/line/bot';
+
+const app = express();
+
+app.use('/line/bot', lineBotRouter);
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+export const api = region('asia-northeast1').https.onRequest(app);
+
+export default app;
